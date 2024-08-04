3f6494a4b21a78a299d28b9fddc0751c4956d2b0
unknown <prudhvirajubh999@gmail.com>
2024-08-04 18:04:38 +0530

    c2

--- output.tf
+++ output.tf
@@ -2,3 +2,14 @@ output "azs" {
   value = slice(data.aws_availability_zones.azs.names,0,2)
 }
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].ids
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].ids
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].ids
+}
